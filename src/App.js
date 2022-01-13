05b3878047ad34c315008fd458b9cf6e15083228
yootaek <eldbxor@naver.com>
2022-01-13 17:53:10 +0900

    useReducer 사용하기

--- src/App.js
+++ src/App.js
@@ -2,21 +2,25 @@ import Counter from "./Counter";
 import Info from "./Info";
 import { useState } from "react";
 
+// const App = () => {
+//   const [visible, setVisible] = useState(false);
+//   return (
+//     <div>
+//       <button
+//         onClick={() => {
+//           setVisible(!visible);
+//         }}
+//       >
+//         {visible ? "숨기기" : "보이기"}
+//       </button>
+//       <hr />
+//       {visible && <Info />}
+//     </div>
+//   );
+// };
+
 const App = () => {
-  const [visible, setVisible] = useState(false);
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? "숨기기" : "보이기"}
-      </button>
-      <hr />
-      {visible && <Info />}
-    </div>
-  );
+  return <Counter />;
 };
 
 export default App;
